2d017ce0343063b3379594b8378ce80bd28a02c3
ThomasSpare <t.spare.jkpg@gmail.com>
2023-05-05 08:17:31 +0000

    admin fields added

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,21 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ['title', 'content']
+    list_display = ('title', 'slug', 'status', 'created_on')
+    list_filter = ('status', 'created_on')
+    summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_field = ['name', 'email', 'body']
